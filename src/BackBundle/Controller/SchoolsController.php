9fd01890899340be0fb6ab0373a9a73111856134
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-04-09 14:08:49 +0200

    add create school action

--- src/BackBundle/Controller/SchoolsController.php
+++ src/BackBundle/Controller/SchoolsController.php
@@ -4,20 +4,60 @@ namespace BackBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Controller\BaseController;
 use BackBundle\Form\SchoolType;
 use AppBundle\Entity\School;
 
-class SchoolsController extends Controller
+/**
+ * @Route("/schools")
+ */
+class SchoolsController extends BaseController
 {
     /**
-     * @Route("/schools/new")
+     * @Route("/new", name="back_schools_new")
      * @Method({"GET"})
      */
     public function indexAction()
     {
         $school = new School();
         $form = $this->createForm(SchoolType::class, $school);
+
+        return $this->render('BackBundle:Schools:new.html.twig', array(
+                'form' => $form->createView()
+            )
+        );
+    }
+
+    /**
+     * @Route("/create", name="back_schools_create")
+     * @Method({"POST"})
+     */
+    public function createAction(Request $request)
+    {
+        $school = new School();
+        $school->setUser($this->getUser());
+        $form = $this->createForm(SchoolType::class, $school);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($school);
+            $em->flush();
+
+            $this->setFlash('notice', 'Auto école créée !');
+
+            return $this->redirect(
+                $this->generateUrl('back_schools_show', array(
+                        'id' => $school->getId()
+                    )
+                )
+            );
+        }
+
+        $this->setFlash('alert', 'Impossible de créer cette auto école');
 
         return $this->render('BackBundle:Schools:new.html.twig', array(
                 'form' => $form->createView()
