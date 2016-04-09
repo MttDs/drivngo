71bb24c4f23509f49ce3724a9c0f99ecb195037e
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-04-09 13:03:13 +0200

    create SchoolType form

---
+++ src/BackBundle/Form/SchoolType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BackBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class SchoolType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('address', TextType::class)
+            ->add('city', TextType::class)
+            ->add('postcode', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\School'
+        ));
+    }
+}
