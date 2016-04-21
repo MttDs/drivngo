57c9e30b1fd5921c59fa8377992348fcf355a532
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-04-21 21:08:31 +0200

    display schools

--- src/AppBundle/Controller/SchoolsController.php
+++ src/AppBundle/Controller/SchoolsController.php
@@ -4,14 +4,27 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class SchoolsController extends Controller
+class SchoolsController extends BaseController
 {
     /**
      * @Route("/auto-ecoles")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('AppBundle:Schools:index.html.twig');
+        $schoolsRepository = $this->getRepository('AppBundle:School');
+        $query = $schoolsRepository->getSchoolsQuery();
+
+        $pagination = $this->getPaginator()->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            10/*limit per page*/
+        );
+
+        return $this->render('AppBundle:Schools:index.html.twig', array(
+                'schools' => $pagination
+            )
+        );
     }
 }
