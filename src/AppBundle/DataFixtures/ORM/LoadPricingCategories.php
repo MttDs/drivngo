766f9e14180bf22e3f7b23c052582afa7d3556a4
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-04-21 08:58:11 +0200

    create LoadPrincingCategories fixture

---
+++ src/AppBundle/DataFixtures/ORM/LoadPricingCategories.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\PricingCategory;
+
+class LoadPricingCategories extends AbstractFixture implements OrderedFixtureInterface {
+
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->getPricingNames() as $pricing_name) {
+            $pricingCategory = new PricingCategory();
+            $pricingCategory->setName($pricing_name);
+
+            $manager->persist($pricingCategory);
+        }
+
+        $manager->flush();
+    }
+
+    private function getPricingNames() {
+        return array(
+            'Tarif inscription'
+        );
+    }
+
+    public function getOrder()
+    {
+        return 20;
+    }
+}
+?>
