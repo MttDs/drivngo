c9fb1e093f7e4d39ad5be2d8c61b9b4d97f25185
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-04-24 15:01:59 +0200

    add changeAction (enable or disable a student)

--- src/BackBundle/Controller/StudentsController.php
+++ src/BackBundle/Controller/StudentsController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 use AppBundle\Controller\BaseController;
 use AppBundle\Entity\School;
+use BackBundle\Entity\Student;
 
 class StudentsController extends BaseController
 {
@@ -19,10 +20,39 @@ class StudentsController extends BaseController
      * @ParamConverter("works_in")
      * @Method({"GET"})
      */
-    public function indexAction(Request $request, School $school) {
+    public function indexAction(Request $request, School $school)
+    {
         return $this->render('BackBundle:Students:index.html.twig', array(
                 'school' => $school
             )
         );
+    }
+
+    /**
+     * @Route("/schools/{school_id}/students/{id}", name="back_schools_students_change", requirements={"school_id" = "\d+", "id" = "\d+"})
+     * @Security("has_role('ROLE_SECRETARY')")
+     * @ParamConverter("works_in")
+     * @ParamConverter("employee", class="BackBundle:Student", options={"id" = "id"})
+     * @Method({"POST"})
+     */
+    public function changeAction(Request $request, School $school, Student $employee)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        if (null !== $request->request->get('active') && $request->request->get('active') == "on"){
+            $this->setFlash('notice', "L'utilisateur ".$employee->getUser()->getFirstname()." ".$employee->getUser()->getLastname() ." a été activé.");
+
+            $employee->setActive(true);
+        }
+        else {
+            $this->setFlash('notice', "L'utilisateur ".$employee->getUser()->getFirstname()." ".$employee->getUser()->getLastname() ." a été désactivé.");
+
+            $employee->setActive(false);
+        }
+
+        $em->persist($employee);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('back_schools_students', array('school_id' => $school->getId())));
     }
 }
