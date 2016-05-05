b31b18095f591b00fb012b777c8cc8d485f9a92b
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-05-05 14:35:32 +0200

    add Employee new action

--- src/BackBundle/Controller/EmployeesController.php
+++ src/BackBundle/Controller/EmployeesController.php
@@ -10,17 +10,35 @@ use Symfony\Component\HttpFoundation\Request;
 
 use AppBundle\Controller\BaseController;
 use AppBundle\Entity\School;
+use UserBundle\Entity\User;
 use BackBundle\Entity\Employees;
+use BackBundle\Form\EmployeeType;
 
 class EmployeesController extends BaseController
 {
     /**
-     * @Route("/schools/{school_id}/employee", name="back_schools_employees", requirements={"school_id" = "\d+"})
-     * @Security("has_role('ROLE_SECRETARY')")
+     * @Route("/schools/{school_id}/employees", name="back_schools_employees", requirements={"school_id" = "\d+"})
+     * @Security("has_role('ROLE_MANAGER')")
      * @ParamConverter("school_manager")
      * @Method({"GET"})
      */
-    public function indexAction(School $school) {
+    public function indexAction(School $school)
+    {
         return $this->render('BackBundle:Employees:index.html.twig', array('school' => $school));
+    }
+
+    /**
+     * @Route("/schools/{school_id}/employees/new", name="back_schools_employees_new", requirements={"school_id" = "\d+"})
+     * @Security("has_role('ROLE_MANAGER')")
+     * @ParamConverter("school_manager")
+     * @Method({"GET"})
+     */
+    public function newAction(School $school)
+    {
+        $form = $this->createForm(EmployeeType::class, new User());
+
+        return $this->render('BackBundle:Employees:new.html.twig', array(
+            'school' => $school,
+            'form'   => $form->createView()));
     }
 }
