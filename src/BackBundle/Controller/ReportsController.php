d14d05b402b243cefee1919c3c2e1f7ca9221618
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-05-11 22:38:20 +0200

    add user vote

--- src/BackBundle/Controller/ReportsController.php
+++ src/BackBundle/Controller/ReportsController.php
@@ -9,8 +9,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 
 use AppBundle\Controller\BaseController;
+use BackBundle\Form\ReportType;
 use AppBundle\Entity\School;
 use BackBundle\Entity\Student;
+use BackBundle\Entity\Report;
+use UserBundle\Entity\User;
 
 class ReportsController extends BaseController
 {
@@ -38,6 +41,64 @@ class ReportsController extends BaseController
         return $this->render('BackBundle:Reports:index.html.twig', array(
                 'school' => $school,
                 'persons' => $persons
+            )
+        );
+    }
+
+    /**
+     * @Route("/schools/{school_id}/reports/{user_id}/new", name="back_schools_reports_new", requirements={"school_id" = "\d+"})
+     * @Security("is_granted('ROLE_USER')")
+     * @ParamConverter("user", class="UserBundle:User", options={"id" = "user_id"})
+     * @ParamConverter("in_school")
+     * @Method({"GET"})
+     */
+    public function newAction(Request $request, School $school, User $user)
+    {
+        $form = $this->createForm(ReportType::class, new Report());
+
+        return $this->render('BackBundle:Reports:new.html.twig', array(
+                'school' => $school,
+                'user' => $user,
+                'form' => $form->createView()
+            )
+        );
+    }
+
+    /**
+     * @Route("/schools/{school_id}/reports/{user_id}/create", name="back_schools_reports_create", requirements={"school_id" = "\d+"})
+     * @Security("is_granted('ROLE_USER')")
+     * @ParamConverter("user", class="UserBundle:User", options={"id" = "user_id"})
+     * @ParamConverter("in_school")
+     * @Method({"POST"})
+     */
+    public function createAction(Request $request, School $school, User $user)
+    {
+        $report = new Report();
+        $report->setUser($user);
+        $report->setVoter($this->getUser());
+        $report->setSchool($school);
+        $report->setCreatedAt(new \DateTime());
+
+        $form = $this->createForm(ReportType::class, $report);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($report);
+            $em->flush();
+
+            $this->setFlash('notice', "Note ajoutée !");
+
+            return $this->redirect($this->generateUrl('back_schools_reports', array('school_id' => $school->getId())));
+        }
+
+        $this->setFlash('alert', "Impossible d'ajouter cette note");
+
+        return $this->render('BackBundle:Reports:new.html.twig', array(
+                'school' => $school,
+                'user' => $user,
+                'form' => $form->createView()
             )
         );
     }
